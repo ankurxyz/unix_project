6026388f78fda4d384b5b0a794f0feab3ed6443c
ankurxyz <ankur01xyz@gmail.com>
2023-04-16 19:39:29 +0530

    guessing game program modified.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Title of Project : **Guessing Game**  
-Date and time at which Make was run is: **Sun Apr 16 00:49:54 IST 2023**  
+Date and time at which Make was run is: **Sun Apr 16 19:38:08 IST 2023**  
 Number of lines of code in guessinggame.sh file is **31**  
--- guessinggame.sh
+++ guessinggame.sh
@@ -3,7 +3,7 @@
 
 #Function computing number of files in current directory
 function no_of_files {
-	number=$(ls -l | egrep "^-" | wc -l)	
+	local number=$(ls -l | egrep "^-" | wc -l)	
 	echo $number
 }
 
@@ -12,15 +12,15 @@ read response
 
 while [[ true ]]
 do
-if [[ $response -eq no_of_files ]]
+	if [[ $response -eq $(no_of_files) ]]
 then
 	echo "Congratulations! You are absolutely correct."
 	break
-elif [[ $response -gt no_of_files ]]
+elif [[ $response -gt $(no_of_files) ]]
 then
 	echo "Your guess is too high! Try again."	
 	read response
-elif [[ $response -lt no_of_files ]]
+elif [[ $response -lt $(no_of_files) ]]
 then
 	echo "Your guess is too low! Try again."
 	read response
